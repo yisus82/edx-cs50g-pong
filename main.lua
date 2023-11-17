fe888d3da028600b3645932c51bf6012a97f3db7
yisus82 <djalma8@gmail.com>
2023-11-17 10:50:22 +0100

    Refactor variables

--- main.lua
+++ main.lua
@@ -12,6 +12,8 @@ VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200
 
+local windowWidth, windowHeight, smallFont, scoreFont, player1Score, player2Score, player1Y, player2Y
+
 --[[
   Runs when the game first starts up, only once; used to initialize the game.
 ]]
@@ -19,7 +21,7 @@ function love.load()
   -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text and graphics
   love.graphics.setDefaultFilter('nearest', 'nearest')
 
-  local windowWidth, windowHeight = love.window.getDesktopDimensions()
+  windowWidth, windowHeight = love.window.getDesktopDimensions()
 
   -- initialize our virtual resolution, which will be rendered within our actual window no matter its dimensions
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, windowWidth, windowHeight, {
@@ -29,21 +31,21 @@ function love.load()
   })
 
   -- more "retro-looking" font object we can use for any text
-  local smallFont = love.graphics.newFont('font.ttf', 8)
+  smallFont = love.graphics.newFont('font.ttf', 8)
 
   -- set LÖVE2D's active font to the smallFont obect
   love.graphics.setFont(smallFont)
 
   -- larger font for drawing the score on the screen
-  ScoreFont = love.graphics.newFont('font.ttf', 32)
+  scoreFont = love.graphics.newFont('font.ttf', 32)
 
   -- initialize score variables, used for rendering on the screen and keeping track of the winner
-  Player1Score = 0
-  Player2Score = 0
+  player1Score = 0
+  player2Score = 0
 
   -- paddle positions on the Y axis (they can only move up or down)
-  Player1Y = 30
-  Player2Y = VIRTUAL_HEIGHT - 50
+  player1Y = 30
+  player2Y = VIRTUAL_HEIGHT - 50
 end
 
 --[[
@@ -54,19 +56,19 @@ function love.update(dt)
   -- player 1 movement
   if love.keyboard.isDown('w') then
     -- add negative paddle speed to current Y scaled by deltaTime
-    Player1Y = Player1Y + -PADDLE_SPEED * dt
+    player1Y = player1Y + -PADDLE_SPEED * dt
   elseif love.keyboard.isDown('s') then
     -- add positive paddle speed to current Y scaled by deltaTime
-    Player1Y = Player1Y + PADDLE_SPEED * dt
+    player1Y = player1Y + PADDLE_SPEED * dt
   end
 
   -- player 2 movement
   if love.keyboard.isDown('up') then
     -- add negative paddle speed to current Y scaled by deltaTime
-    Player2Y = Player2Y + -PADDLE_SPEED * dt
+    player2Y = player2Y + -PADDLE_SPEED * dt
   elseif love.keyboard.isDown('down') then
     -- add positive paddle speed to current Y scaled by deltaTime
-    Player2Y = Player2Y + PADDLE_SPEED * dt
+    player2Y = player2Y + PADDLE_SPEED * dt
   end
 end
 
@@ -85,15 +87,15 @@ function love.draw()
 
   -- draw score on the left and right center of the screen
   -- need to switch font to draw before actually printing
-  love.graphics.setFont(ScoreFont)
-  love.graphics.print(tostring(Player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-  love.graphics.print(tostring(Player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+  love.graphics.setFont(scoreFont)
+  love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+  love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
   -- render first paddle (left side)
-  love.graphics.rectangle('fill', 10, Player1Y, 5, 20)
+  love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
   -- render second paddle (right side)
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, Player2Y, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
   -- render ball (center)
   love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
