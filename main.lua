8ac8ee68f6b70094181e8d3b2280ccb4b426a9f3
yisus82 <djalma8@gmail.com>
2023-11-17 10:05:35 +0100

    Fix global variables casing

--- main.lua
+++ main.lua
@@ -35,15 +35,15 @@ function love.load()
   love.graphics.setFont(smallFont)
 
   -- larger font for drawing the score on the screen
-  scoreFont = love.graphics.newFont('font.ttf', 32)
+  ScoreFont = love.graphics.newFont('font.ttf', 32)
 
   -- initialize score variables, used for rendering on the screen and keeping track of the winner
-  player1Score = 0
-  player2Score = 0
+  Player1Score = 0
+  Player2Score = 0
 
   -- paddle positions on the Y axis (they can only move up or down)
-  player1Y = 30
-  player2Y = VIRTUAL_HEIGHT - 50
+  Player1Y = 30
+  Player2Y = VIRTUAL_HEIGHT - 50
 end
 
 --[[
@@ -54,19 +54,19 @@ function love.update(dt)
   -- player 1 movement
   if love.keyboard.isDown('w') then
     -- add negative paddle speed to current Y scaled by deltaTime
-    player1Y = player1Y + -PADDLE_SPEED * dt
+    Player1Y = Player1Y + -PADDLE_SPEED * dt
   elseif love.keyboard.isDown('s') then
     -- add positive paddle speed to current Y scaled by deltaTime
-    player1Y = player1Y + PADDLE_SPEED * dt
+    Player1Y = Player1Y + PADDLE_SPEED * dt
   end
 
   -- player 2 movement
   if love.keyboard.isDown('up') then
     -- add negative paddle speed to current Y scaled by deltaTime
-    player2Y = player2Y + -PADDLE_SPEED * dt
+    Player2Y = Player2Y + -PADDLE_SPEED * dt
   elseif love.keyboard.isDown('down') then
     -- add positive paddle speed to current Y scaled by deltaTime
-    player2Y = player2Y + PADDLE_SPEED * dt
+    Player2Y = Player2Y + PADDLE_SPEED * dt
   end
 end
 
@@ -85,15 +85,15 @@ function love.draw()
 
   -- draw score on the left and right center of the screen
   -- need to switch font to draw before actually printing
-  love.graphics.setFont(scoreFont)
-  love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-  love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+  love.graphics.setFont(ScoreFont)
+  love.graphics.print(tostring(Player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+  love.graphics.print(tostring(Player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
   -- render first paddle (left side)
-  love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+  love.graphics.rectangle('fill', 10, Player1Y, 5, 20)
 
   -- render second paddle (right side)
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, Player2Y, 5, 20)
 
   -- render ball (center)
   love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
