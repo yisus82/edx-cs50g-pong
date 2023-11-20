c27ecc9704d1ff685e0fbcaf339c2d1fa8d56fb6
yisus82 <djalma8@gmail.com>
2023-11-20 10:03:20 +0100

    Add ball collisions

--- main.lua
+++ main.lua
@@ -125,11 +125,50 @@ function love.update(dt)
     player2.dy = 0
   end
 
-  -- if we are in play state, update positions based on velocity scaled by deltaTime
+  -- if we are in play state, check collisions and update positions based on velocity scaled by deltaTime
   if gameState == 'play' then
+    -- detect ball collision with paddles, reversing dx if true and
+    -- slightly increasing it, then altering the dy based on the position of collision
+    if ball:collides(player1) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player1.x + 5
+
+      -- keep velocity going in the same direction, but randomize it
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+    if ball:collides(player2) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player2.x - 4
+
+      -- keep velocity going in the same direction, but randomize it
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+
+    -- detect upper and lower screen boundary collision and reverse if collided
+    if ball.y <= 0 then
+      ball.y = 0
+      ball.dy = -ball.dy
+    end
+
+    -- -4 to account for the ball's size
+    if ball.y >= VIRTUAL_HEIGHT - 4 then
+      ball.y = VIRTUAL_HEIGHT - 4
+      ball.dy = -ball.dy
+    end
+
+    -- update positions based on velocity scaled by deltaTime
     ball:update(dt)
     player1:update(dt)
     player2:update(dt)
+
     -- set the title of our application window
     love.window.setTitle('Pong - FPS: ' .. tostring(love.timer.getFPS()))
   else
